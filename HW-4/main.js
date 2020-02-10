50342ef8a055e19900e94326506f4b6582a805b6
Mathew Peters <46757581+Mowshi@users.noreply.github.com>
2020-02-10 07:51:32 -0700

    worked more

--- HW-4/main.js
+++ HW-4/main.js
@@ -10,14 +10,25 @@ function getCh1Choice()
     document.getElementById("choice2").style.display="block";
     document.getElementById("goBotton2").style.display="block";
 
-    myQuestion.innerHTML = "boombaclap";
+    document.getElementById("testboy")style.display="block";
   }
   else if(myChoice === "Ihop")
   {
-    myQuestion.innerHTML = "boomb";
+    myQuestion.innerHTML = "After sitting down in a booth with the least amount of crumbs on it, you place your order and receive your oder of lukewarm food.<br>Suddenly, a squad of cyborgs drop down from the celing and suround you. You panic and look for a way out of the situation, but upon closer examination of the cyborgs,<br>you notice they are lactose intolerance. You look down at your plate and remeber the glass of milk you ordered. Do you fight them off with the milk or run for the exit?";
   }
   else
   {
-    myQuestion.innerHTML = "boombasssslap";
+    myQuestion.innerHTML = "That wasn't an option....";
+  }
+}
+
+
+function getCh1Choice2()
+{
+  var myChoice = document.getElementById("choice").value;
+  var myQuestion = document.getElementById("question");
+  if(myChoice2 === "")
+  {
+
   }
 }
