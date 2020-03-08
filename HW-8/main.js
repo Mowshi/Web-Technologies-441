e16c9dc3573a132c251b672f6fa1344e4fdcb3ff
Mathew Peters <46757581+Mowshi@users.noreply.github.com>
2020-03-08 17:30:13 -0600

    finished

--- HW-8/main.js
+++ HW-8/main.js
@@ -1,8 +1,70 @@
 $(document).ready(function(){
-
+  $(".boos").hide();
+  $("h2").hide();
   $("button").click(function(){
-    $("")
+    $("h2").fadeIn();
+    for(var i = 0; i < 4; i++)
+    {
+      moveBoos();
+    }
+
   });
 
+});
+
+function moveBoos()
+{
+  $(".boos").fadeIn().animate({height: '10px'}, "slow").animate({height:'30px'},"slow");
+
+}
+
+var dogSelector = "#dog";
+var allDogs = new Array();
+class DogInfo{
+    constructor(selector, imagePath)
+    {
+        this.selector = selector;
+        this.imagePath = imagePath;
+    }
+
+    get theSelector(){
+        return this.selector;
+    }
+
+    get theImagePath(){
+        return this.imagePath;
+    }
+
+    toString()
+    {
+        return this.selector + ":" + this.imagePath;
+    }
+}
+
+function initializeArray()
+{
+    var dog = new DogInfo("#dog", "imgs/dog.jpg",);
+    allDogs.push(dog);
+
+}
+$(document).ready(function(){
+    console.log(allDogs[0].toString());
+    console.log(allDogs[0].theSelector);
+    console.log(allDogs[0].theImagePath);
+
+    //$(allDogs[0].theSelector).src = allDogs[0].theImagePath;
+    //$("#my_image").attr("src","second.jpg");
+    $(allDogs[0].theSelector).attr("src", allDogs[0].theImagePath);
+
+    $("button").click(function(){
+
+        $(".stuff").fadeOut();
+
+        $("#third").toggle();
+          // setInterval(moveSquare, 1000);
+
+       // $(allDogs[0].theSelector).fadeOut().fadeIn();
+
+    });
 
 });
